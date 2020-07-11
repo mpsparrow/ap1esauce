f3fd18733207a1756e7ee4b95d9aa35461ca40c3
Matthew <matthew@younginwp.com>
2020-07-11 01:18:13 -0400

    debug

--- socket.py
+++ socket.py
@@ -1,8 +1,8 @@
-import socket
+import socket as sckt
 from settings import HOST, PORT, PASS, IDENT, CHANNEL
 
 def openSocket():
-	s = socket.socket()
+	s = sckt.socket()
 	s.connect((HOST, PORT))
 	s.send("PASS {}\r\n".format(PASS).encode("utf-8"))
 	s.send("NICK {}\r\n".format(NICK).encode("utf-8"))
