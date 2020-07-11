7b6db7804d0022c900d92d1ffa9b9cfe731c93ae
Matthew <matthew@younginwp.com>
2020-07-11 01:14:51 -0400

    tab fix

--- run.py
+++ run.py
@@ -8,9 +8,10 @@ s = openSocket()
 
 while True:
     response = s.recv(1024).decode("utf-8")
+    
     if response == "PING :tmi.twitch.tv\r\n":
         s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
     else:
         print(response)
-	
-	sleep(1 / RATE)
+
+    sleep(1 / RATE)
