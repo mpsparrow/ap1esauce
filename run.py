bc6ce8fdf290590524b4300ff44abcb88f466981
Matthew <matthew@younginwp.com>
2020-07-11 02:23:25 -0400

    troubleshooting

--- run.py
+++ run.py
@@ -1,46 +1,25 @@
-import os
 from twitchio.ext import commands
 from settings import PASS, CLIENTID, IDENT, PREFIX, CHANNEL
 
-# set up the bot
-bot = commands.Bot(
-    irc_token=PASS,
-    client_id=CLIENTID,
-    nick=IDENT,
-    prefix=PREFIX,
-    initial_channels=CHANNEL
-)
+class Bot(commands.Bot):
 
-@bot.event
-async def event_ready():
-    'Called once when the bot goes online.'
-    print(f"{IDENT} is online!")
-    ws = bot._ws  # this is only needed to send messages within event_ready
-    await ws.send_privmsg(CHANNEL, f"/me has landed!")
+    def __init__(self):
+        super().__init__(irc_token=PASS, client_id=CLIENTID, nick=IDENT, prefix=PREFIX,
+                         initial_channels=[CHANNEL])
 
+    # Events don't need decorators when subclassed
+    async def event_ready(self):
+        print(f'Ready | {self.nick}')
 
-@bot.event
-async def event_message(ctx):
-    'Runs every time a message is sent in chat.'
-    print("run")
-    print(ctx.content)
-    
-    # make sure the bot ignores itself and the streamer
-    if ctx.author.name.lower() == IDENT.lower():
-        return
+    async def event_message(self, message):
+        print(message.content)
+        await self.handle_commands(message)
 
-    await bot.handle_commands(ctx)
-
-    # await ctx.channel.send(ctx.content)
-
-    if 'hello' in ctx.content.lower():
-        await ctx.channel.send(f"Hi, @{ctx.author.name}!")
+    # Commands use a different decorator
+    @commands.command(name='test')
+    async def my_command(self, ctx):
+        await ctx.send(f'Hello {ctx.author.name}!')
 
 
-@bot.command(name='test')
-async def test(ctx):
-    await ctx.send('test passed!')
-
-
-if __name__ == "__main__":
-    bot.run()
+bot = Bot()
+bot.run()
