253f15e3c96b1820af4c0e548deb319dbe3e97df
Matthew <matthew@younginwp.com>
2020-07-13 02:02:59 -0400

    debug

--- run.py
+++ run.py
@@ -20,6 +20,7 @@ class Bot(commands.Bot):
 
     async def event_message(self, message):
         try:
+            print(f"{message.channel} | {self.get_stream(message.channel)}")
             data = await self.get_stream(message.channel)
             if data is not None:
                 print("0")
