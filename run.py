838f933fdd27d590b31ace2739a70d45304fa9ad
Matthew <matthew@younginwp.com>
2020-07-13 00:34:08 -0400

    log messages test

--- run.py
+++ run.py
@@ -1,24 +1,22 @@
 from twitchio.ext import commands
 from settings import PASS, CLIENTID, IDENT, PREFIX, CHANNEL
 
-class Bot(commands.Bot):
+def write(filename: str, msg: str):
+    log = open(f'{filename}','a+')
+    log.write(f"{msg}\n")
+    log.close()
 
+class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(irc_token=PASS, client_id=CLIENTID, nick=IDENT, prefix=PREFIX,
-                         initial_channels=CHANNEL)
+        super().__init__(irc_token=PASS, client_id=CLIENTID, nick=IDENT, prefix=PREFIX, initial_channels=CHANNEL)
 
     # Events don't need decorators when subclassed
     async def event_ready(self):
         print(f'Ready | {self.nick}')
 
     async def event_message(self, message):
-        print(f"{message.channel} | {message.author.name}: {message.content}")
+        write(f"{message.channel}", message.content)
         await self.handle_commands(message)
-
-    # Commands use a different decorator
-    @commands.command(name='testing')
-    async def my_command(self, ctx):
-        await ctx.send(f'Hello {ctx.author.name}!')
 
 bot = Bot()
 bot.run()
